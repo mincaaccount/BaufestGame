29c59ae2c7114b7c455711b926e7eb7ff6d56301
Marcos Inca <marcosezequiel.inca.contractor@gdevtools.com>
2020-11-16 09:40:25 -0300

    Enemigo golpeable

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -8,3 +8,8 @@ func _physics_process(delta):
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+func _on_Bullet_area_entered(area):
+	if area.is_in_group("damageable"):
+		area.damage(5)
+		queue_free()
