734a58298b2fa7ad194bea333d81420f87ab36fe
Marcos Inca <marcosezequiel.inca.contractor@gdevtools.com>
2020-11-19 17:31:01 -0300

    Correción de Movimiento Enemy

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -3,7 +3,7 @@ extends Area2D
 export var life: int = 20
 var player = null
 var move = Vector2.ZERO
-export var speed: int = 1
+export var speed: int = 50
 
 func damage(amount: int):
 	life -= amount
@@ -14,18 +14,17 @@ func _physics_process(delta):
 	move = Vector2.ZERO
 	
 	if player != null:
-		move = position.direction_to(player.position) * speed
+		move = position.direction_to(player.position)
 	else:
 		move = Vector2.ZERO
 		
 	move = move.normalized()
+	position.x += move.x * speed
+	
 
-func _on_Area2D_body_entered(body):
-	if body != self:
-		player = body
+func _on_Area2D_area_entered(body):
+		if body != self:
+			player = body
 
-func _on_Area2D_body_exited(body):
+func _on_Area2D_area_exited(area):
 	player = null
-
-
-
