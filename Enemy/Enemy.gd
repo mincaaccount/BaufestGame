a5ceb60ba4cc2950814bb6b6de3511ab141c0a3d
Marcos Inca <marcosezequiel.inca.contractor@gdevtools.com>
2020-11-19 10:13:54 -0300

    Movimiento Enemigo

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,8 +1,31 @@
 extends Area2D
 
 export var life: int = 20
+var player = null
+var move = Vector2.ZERO
+export var speed: int = 1
 
 func damage(amount: int):
 	life -= amount
 	if life <= 0:
 		queue_free()
+
+func _physics_process(delta):
+	move = Vector2.ZERO
+	
+	if player != null:
+		move = position.direction_to(player.position) * speed
+	else:
+		move = Vector2.ZERO
+		
+	move = move.normalized()
+
+func _on_Area2D_body_entered(body):
+	if body != self:
+		player = body
+
+func _on_Area2D_body_exited(body):
+	player = null
+
+
+
